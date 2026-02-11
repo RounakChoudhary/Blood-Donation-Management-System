15cdd2db15820ac91930ec2e0396bd11a1363091
teenujyani <kumari540teenu@gmail.com>
2026-02-11 16:11:35 +0530

    feat: adding initial UI structure

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="text-amber-50">
+      <div className="mt-70 flex justify-center">
+        <h1>Bllod-Donation-Managment-System</h1>
+      </div>
+    </div>
+  );
+};
+
+export default App;
