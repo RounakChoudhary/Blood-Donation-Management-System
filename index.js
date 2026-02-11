136b84c63686874b549dcbc49d525d4034429d84
RounakChoudhary <rounakchoudhary2005@gmail.com>
2026-02-11 15:47:54 +0530

    chore: auth flow integration and cleanup

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
-require("./models/db");
-const pool = require("./models/db");
 
-app.get("/health", async (req, res) => {
-  const result = await pool.query("SELECT current_database() db, NOW() time");
-  res.json(result.rows[0]);
-});
-
+require("./models/db"); 
 
 app.use(express.json());
 
-app.listen(PORT, ()=>{
-    console.log(`Server running on port ${PORT}`);
-})
+// Routes
+const authRoutes = require("./routes/auth.routes");
+const userRoutes = require("./routes/user.routes");
+
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
