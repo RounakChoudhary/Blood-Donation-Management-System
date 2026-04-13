0784538b8a74e30fce379236c987150b8739540a
RounakChoudhary <rounakchoudhary2005@gmail.com>
2026-04-14 00:42:44 +0530

    feat(requests): add regular blood request status tracking

--- routes/bloodBank.routes.js
+++ routes/bloodBank.routes.js
@@ -9,6 +9,8 @@ const bloodBankController = require("../controllers/bloodBank.controller");
 router.get("/nearby", auth, bloodBankController.getNearby);
 router.get("/dashboard", bloodBankAuth, bloodBankController.getDashboard);
 router.post("/inventory/adjustments", bloodBankAuth, bloodBankController.adjustInventory);
+router.get("/regular-requests", hospitalAuth, bloodBankController.listRegularRequests);
+router.get("/regular-requests/:id", hospitalAuth, bloodBankController.getRegularRequestDetails);
 router.post("/regular-requests", hospitalAuth, bloodBankController.createRegularRequest);
 
 module.exports = router;
